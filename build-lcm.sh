40b872060dcf09cfe56e8498cb8413f5f117f957
Francesco Branca <francesco.branca@dfki.de>
2022-12-09 18:51:38 +0100

    motor parallel process test

--- build-lcm.sh
+++ build-lcm.sh
@@ -3,5 +3,8 @@ lcm-gen -xp robot_command.lcm
 lcm-gen -xp robot_state.lcm
 lcm-gen -xp gripper_command.lcm
 lcm-gen -xp gripper_state.lcm
+cd ..
+cd motor_messages
+lcm-gen -p motor_state.lcm
 cd ../..
 echo "LCM bindings built successfully!"
--- src/motor_driver/motor_controller.py
+++ src/motor_driver/motor_controller.py
@@ -2,20 +2,25 @@ import time
 import numpy as np
 from motor_driver.canmotorlib import CanMotorController
 import matplotlib.pyplot as plt
+from motor_messages.motorlcm import motor_state
+import lcm
 
 can_port = 'can0'
 motor_id = 12
 motor_01 = CanMotorController(can_port, motor_id, motor_type="AK80_9_V1p1")
 
 class motor_controller:
-    def __init__(self, can_port, motor_id, motor_type="AK80_9_V1p1", plot=False):
+    def __init__(self, can_port, motor_id, motor_type="AK80_9_V1p1", plot=False, channel = "MOTOR_STATE"):
         self.plot = plot
+        self.channel = channel
+        self.lc = lcm.LCM()
         self.motor = CanMotorController(can_port, motor_id, motor_type=motor_type)
         self.motor.enable_motor()
         self.loop()
 
     def loop(self):
         start = time.time()
+        mst = motor_state()
         i = 0
         n = 10000
         time_ = []
@@ -30,11 +35,6 @@ class motor_controller:
             else:
                 pos, vel, tau = self.motor.send_deg_command(0, 20, 0, 5, 0)
 
-            time_.append(time.time()-start)
-            pos_.append(pos)
-            vel_.append(vel)
-            tau_.append(tau)
-
             # filter
             filter = 20
             filter_size = filter-1
@@ -43,9 +43,19 @@ class motor_controller:
                 #vel_filtered = (vel_[i] + vel_[i-1] + vel_[i-2] + vel_[i-3] + vel_[i-4])/5
             else:
                 vel_filtered = 0
-            #vel_[i] = vel_filtered
-            vel_f.append(vel_filtered)
             i+=1
+            
+            mst.motor_pos = pos
+            mst.motor_vel = vel
+            mst.motor_tau = tau
+            self.lc.publish(self.channel, mst.encode())
+
+            if self.plot == True:
+                time_.append(time.time()-start)
+                pos_.append(pos)
+                vel_.append(vel)
+                vel_f.append(vel_filtered)
+                tau_.append(tau)    
 
         print("Disabling Motors...")
         self.motor.send_deg_command(0, 0, 0, 0, 0)
@@ -62,6 +72,6 @@ class motor_controller:
 if __name__=="__main__":
     can_port = 'can0'
     motor_id = 12
-    motor_controller(can_port, motor_id, plot=True)
+    motor_controller(can_port, motor_id, plot=False)
 
     
