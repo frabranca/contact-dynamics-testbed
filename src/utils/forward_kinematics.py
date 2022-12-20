4d1bdad8075622a054da982e094b6993b57ef72c
Francesco Branca <francesco.branca@dfki.de>
2022-12-20 11:21:50 +0100

    forward kinematics

--- src/utils/forward_kinematics.py
+++ src/utils/forward_kinematics.py
@@ -1,11 +1,13 @@
 from math import *
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class ForwardKinematics:
     def __init__(self, theta_list):
         self.df = self.dataframe(theta_list)
-        self.tm = np.round(self.tm(),4)
+        self.tm = self.tmatrix()[0]
+        self.xyz = self.tmatrix()[1]
 
     def dataframe(self, theta_list):
         theta_list.append(0)
@@ -25,23 +27,39 @@ class ForwardKinematics:
                         [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha)],
                         [0, np.sin(alpha), np.cos(alpha)]])
         tran = np.array([[a*np.cos(theta),a*np.sin(theta),d]]).T
-
         zero_rot = np.eye(3)
         zero_tran = np.zeros((3,1))
-        
         last_row = np.array([[0, 0, 0, 1]])
-
         Rotation    = np.vstack((np.hstack((rot, zero_tran)), last_row))
         Translation = np.vstack((np.hstack((zero_rot, tran)), last_row))
         return Rotation @ Translation
     
-    def tm(self):
+    def tmatrix(self):
         tmi = np.eye(4)
+        xyz = []
         for i in self.df.index:
             tmi = tmi @ self.T(self.df["theta"][i], self.df["alpha"][i], self.df["a"][i], self.df["d"][i])
-        return tmi
+            xyzi = np.round(tmi,4)[0:3,3:4].T[0]
+            xyz.append(xyzi)
+        xyz = np.array(xyz)
+        tmi = np.round(tmi,4)
+        return tmi, xyz
 
-init_pose = [0, -pi/4, 0, -3*pi/4, 0, pi/2, pi/4]
+# init_pose = [0, -pi/4, 0, -3 * pi/4, 0, pi/2, pi/4]
+init_pose = [0,0,pi/4,0,0,0,0]
 
 fk = ForwardKinematics(init_pose)
-print(fk.tm)
+x = fk.xyz[:,0]
+y = fk.xyz[:,1]
+z = fk.xyz[:,2]
+
+ax = plt.axes(projection='3d')
+ax.plot3D(x,y,z)
+ax.set_xlim([0,1])
+ax.set_ylim([0,1])
+ax.set_zlim([0,1])
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+plt.show()
+
