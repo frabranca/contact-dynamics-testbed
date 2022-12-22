6925e2579743b3b7c2342641716309d9441d7141
Francesco Branca <francesco.branca@dfki.de>
2022-12-22 15:29:06 +0100

    time matching

--- src/utils/time_matching.cpp
+++ src/utils/time_matching.cpp
@@ -1,67 +1,83 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
 #include <cmath>
 #include <iostream>
 
-#include <franka/duration.h>
 #include <franka/exception.h>
-#include <franka/rate_limiting.h>
 #include <franka/robot.h>
 #include <franka/gripper.h>
 
 #include "common_functions.cpp"
-#include <algorithm>
-#include <chrono>
-#include<vector>
-using namespace std;
-using namespace std::chrono;
 
-// For demonstration purpose, we will fill up
-// a vector with random integers and then sort
-// them using sort function. We fill record
-// and print the time required by sort function
-int main()
-{
+/**
+ * @example generate_cartesian_pose_motion.cpp
+ * An example showing how to generate a Cartesian motion.
+ *
+ * @warning Before executing this example, make sure there is enough space in front of the robot.
+ */
 
-    // Connect to robot.
+int main(int argc, char** argv) {
+  try {
     franka::Robot robot("192.168.131.40");
     franka::Gripper gripper("192.168.131.40");
     setDefaultBehavior(robot);
 
     // First move the robot to a suitable joint configuration
     std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-    std::array<double, 7> q_grasp = {{M_PI/2, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-
     MotionGenerator motion_generator(0.5, q_goal);
-    MotionGenerator motion_contact_point(0.5, q_grasp);
-
-    robot.setCollisionBehavior(
-    {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, 
-    {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, 
-    {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, 
-    {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, 
-    {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, 
-    {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, 
-    {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, 
-    {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}); 
-
-    franka::Duration period;
-    running = true;
-    double time = 0.0;
+    std::cout << "WARNING: This example will move the robot! "
+              << "Please make sure to have the user stop button at hand!" << std::endl
+              << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
     robot.control(motion_generator);
+    std::cout << "Finished moving to initial joint configuration." << std::endl;
 
-	while (running){
-        
-        if (time == 0.0){
-            robot.control(motion_contact_point);
-        };
-        if (time == 0.809){
-            gripper.grasp(0.01, 10, 60);
-        }
-        time += period.toSec();
-    };
+    // Set additional parameters always before the control loop, NEVER in the control loop!
+    // Set collision behavior.
+    robot.setCollisionBehavior(
+        {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}},
+        {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}}, {{20.0, 20.0, 18.0, 18.0, 16.0, 14.0, 12.0}},
+        {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}},
+        {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}, {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}});
 
-	// Move robot
-    robot.control(motion_contact_point);
-    gripper.grasp(0.01, 10, 60);
+    std::array<double, 16> initial_pose;
+    double time = 0.0;
+    robot.control([&time, &initial_pose](const franka::RobotState& robot_state,
+                                         franka::Duration period) -> franka::CartesianPose {
+      time += period.toSec();
 
-	return 0;
+      if (time == 0.0) {
+        initial_pose = robot_state.O_T_EE_c;
+      }
+
+      if (time == 0.809) {
+        gripper.grasp(0.01, 0.5, 60);
+      }
+
+      std::cout << time << ",";
+      std::cout << robot_state.O_T_EE_c[12] << ",";
+      std::cout << robot_state.O_T_EE_c[13] << ",";
+      std::cout << robot_state.O_T_EE_c[14] << std::endl;      
+
+      constexpr double kRadius = 0.3;
+      double angle = M_PI / 4 * (1 - std::cos(M_PI / 2.0 * time));
+      double delta_x = kRadius * std::sin(angle);
+      double delta_z = kRadius * (std::cos(angle) - 1);
+
+      std::array<double, 16> new_pose = initial_pose;
+      new_pose[12] += delta_x;
+      new_pose[14] += delta_z;
+
+      if (time >= 10.0) {
+        std::cout << std::endl << "Finished motion, shutting down example" << std::endl;
+        return franka::MotionFinished(new_pose);
+      }
+      return new_pose;
+    });
+  } catch (const franka::Exception& e) {
+    std::cout << e.what() << std::endl;
+    return -1;
+  }
+
+  return 0;
 }
