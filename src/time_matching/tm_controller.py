01b533a473b2a2cb7f419e15264f5093f91372cb
Francesco Branca <francesco.branca@dfki.de>
2023-01-17 11:41:19 +0100

    save output

--- src/time_matching/tm_controller.py
+++ src/time_matching/tm_controller.py
@@ -2,21 +2,58 @@ import lcm
 import time
 import matplotlib.pyplot as plt
 import numpy as np
-from robot_messages.frankalcm import robot_command, gripper_command
+from robot_messages.frankalcm import robot_command, gripper_command, robot_state
 from robot_messages.motorlcm import motor_command
 
 class Controller:
-    def __init__(self, rcm_channel, gcm_channel, mcm_channel):
+    def __init__(self, rcm_channel, gcm_channel, mcm_channel, rst_channel, save_data = True):
         self.rcm_channel = rcm_channel
         self.gcm_channel = gcm_channel
         self.mcm_channel = mcm_channel
+        self.rst_channel = rst_channel
+
+        self.save_data = save_data
+
+        # robot states
+        self.q = 0
+        self.q_d = 0
+        self.dq = 0
+        self.dq_d = 0
+        self.ddq_d = 0
+        self.tau_J = 0
+        self.tau_J_d = 0
+        self.dtau_J = 0
+
+        self.q_save = []
+        self.q_d_save = []
+        self.dq_save = []
+        self.dq_d_save = []
+        self.ddq_d_save = []
+        self.tau_J_save = []
+        self.tau_J_d_save = []
+        self.dtau_J_save = []
 
         self.lc = lcm.LCM()
+        self.robot_sub = self.lc.subscribe(self.rst_channel, self.robot_handler)
+
         self.control_loop()
+
+        self.lc.unsubscribe(self.robot_sub)
         
     def message(self, string):
         print("controller.py: " + string)
-
+    
+    def robot_handler(self, channel, data):
+        rst = robot_state.decode(data)
+        self.q             = rst.q
+        self.q_d           = rst.q_d
+        self.dq            = rst.dq
+        self.dq_d          = rst.dq_d
+        self.ddq_d         = rst.ddq_d
+        self.tau_J         = rst.tau_J
+        self.tau_J_d       = rst.tau_J_d
+        self.dtau_J        = rst.dtau_J
+        
     def control_loop(self):
         start = time.time()
         gcm_sent = False
@@ -34,6 +71,16 @@ class Controller:
         #gripper_time = 1. + satellite_time - 0.5# - 0.6523
 
         while (time.time()-start) <= 15.:
+            self.lc.handle()
+            if self.save_data:
+                self.q_save.append(self.q)
+                self.q_d_save.append(self.q_d)
+                self.dq_save.append(self.dq_save)
+                self.dq_d_save.append(self.dq_d)
+                self.ddq_d_save.append(self.ddq_d)
+                self.tau_J_save.append(self.tau_J)
+                self.tau_J_d_save.append(self.tau_J_d)
+                self.dtau_J_save.append(self.dtau_J_save)
 
             if (time.time()-start) >= gripper_time and gcm_sent == False:
                 gcm = gripper_command()
@@ -59,4 +106,5 @@ class Controller:
 if __name__ == "__main__":
     controller = Controller("ROBOT COMMAND", 
                             "GRIPPER COMMAND", 
-                            "MOTOR_COMMAND")
+                            "MOTOR_COMMAND",
+                            "ROBOT STATE")
